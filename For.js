51b0f918639d2646fed55ef9fd53bc1854dc2702
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-07 10:31:00 +0530

    Add files via upload

---
+++ For.js
@@ -0,0 +1,5 @@
+var Week = [11, 21, 51, 101, 111];
+var Cnt = 0;
+for (Cnt = 0; Cnt < Week.length; Cnt++) {
+    console.log(Week[Cnt]);
+}
---
+++ FunDemo.ts
@@ -0,0 +1,20 @@
+
+// Positional parameters
+
+function Display(No1 : number, No2 : number) : void
+{
+    console.log("Value of No1 is : "+No1)
+    console.log("Value of No2 is : "+No2)
+}
+
+
+var A : number = 10
+var B : number = 100
+
+Display(A,B)
+
+Display(11,12)
+
+
+
+
